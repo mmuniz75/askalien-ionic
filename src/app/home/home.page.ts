b55b6ecd13ba2eb8c3407e130260c7186498f0d0
Marcelo <marcelo.muniz@platformbuilders.io>
2019-05-28 16:57:35 -0300

    pagina de questoes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,15 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private router: Router) {}
+  
   
   onClose(){
     if(navigator && navigator['app'])
       navigator['app'].exitApp();
+  }
+
+  onSearch(){
+    this.router.navigateByUrl('/questions');
   }
 }
